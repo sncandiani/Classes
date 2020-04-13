501dfaa286d103b8c088a150b3675607fd50563a
sncandiani <sofiancandiani@gmail.com>
2020-04-13 15:43:09 -0500

    created a class Pizza which prints orders, adds toppings, and initializes pizzas

---
+++ main.py
@@ -0,0 +1,7 @@
+from classes import Pizza
+
+vegetarian = Pizza("16-inch", "deep-dish", "Mushrooms")
+meat_lover = Pizza("24-inch", "thin crust", "sausage")
+
+vegetarian.print_order()
+meat_lover.print_order()
